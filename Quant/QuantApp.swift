a49001cb83389d2ab03fc973791d106b049d34df
dwpope <dwpopesyd@gmail.com>
2025-12-27 01:53:33 +0000

    Initial Commit

---
+++ Quant/QuantApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuantApp.swift
+//  Quant
+//
+//  Created by Learning on 27/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct QuantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
